6ed0b75433051ff12166a496c65d461203a95e37
willtsuchida <101433197+willtsuchida@users.noreply.github.com>
2022-08-11 12:46:42 -0300

    Add files via upload

---
+++ Bee1003SimpleSum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bee1003SimpleSum {
+    public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int SOMA = a + b;
+      System.out.println("SOMA = "+ SOMA);
+
+    }
+}
