0660059e2d91487411a48cad931123806fdfc1fd
willtsuchida <101433197+willtsuchida@users.noreply.github.com>
2022-08-11 12:47:19 -0300

    Add files via upload

---
+++ Bee1004SimpleProduct.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Bee1004SimpleProduct {
+    public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int PROD = a * b;
+
+
+      System.out.println("PROD = " + PROD);
+    }
+}
